f658f438c0ee19ba23434b926ce467ba06382fc1
Diego Pérez <eazel7@gmail.com>
2014-10-19 20:12:39 -0300

    removing app files as they go into packages

--- app/public/scripts/appSocket.js
+++
@@ -1,6 +0,0 @@
-angular.module('appSocket', ['btford.socket-io'])
-.factory('appSocket', function (socketFactory, boxName) {
-  var appSocket = io.connect('/' + boxName);
-  return socketFactory({ioSocket: appSocket});
-});
-
