39d8000103ae18f9ea6442d85128e114ae6f4438
Diego Pérez <eazel7@gmail.com>
2014-10-22 19:14:53 -0300

    small fix

--- api-mongo/packages.js
+++ api-mongo/packages.js
@@ -46,6 +46,9 @@ module.exports = function (config, callback) {
                 .findOne({
                   name: packageName
                 }, function (err, doc) {
+                  console.error("Skip missing package " + packageName);
+                  return callback();
+                
                   var paths = [];
                   if (doc.angularModules) {
                     for (var i = 0; i < doc.angularModules.length; i++) {
