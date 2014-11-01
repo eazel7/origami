63d36029cce87543a5ae438464e90441100334e6
Diego Pérez <eazel7@gmail.com>
2014-11-01 12:40:42 -0300

    config para openshift

--- manager/config.js
+++ manager/config.js
@@ -2,4 +2,22 @@ var config = require('./config-default');
 
 config.root = __dirname;
 
+if (process.env.OPENSHIFT_APPNAME) {
+  var extend = require('util')._extend;
+
+  var newMongo = {
+    database: process.env.OPENSHIFT_APPNAME,
+    host: process.env.OPENSHIFT_MONGODB_DB_HOST,
+    port: process.env.OPENSHIFT_MONGODB_DB_PORT,
+    username: process.env.OPENSHIFT_MONGODB_DB_USERNAME,
+    password: process.env.OPENSHIFT_MONGODB_DB_PASSWORD
+  };
+
+  extend(config.auth['origami-auth-local'].mongo, newMongo);
+  extend(config.mongo, newMongo); 
+  extend(config.mongoSessions, newMongo);
+  config.protocol = 'http';
+  config.prefix = 'https://' + process.env.OPENSHIFT_APPNAME + '.rhcloud.com/';
+}
+
 module.exports = config;
