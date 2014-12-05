538ddd39b2dd6e3a4bc90e3ef9ef83a412d603ec
Diego Perez <eazel7@gmail.com>
2014-12-05 17:44:25 +0000

    removed file from somewhere else

--- index.js
+++
@@ -1,8 +0,0 @@
-var SandboxedModule = require('sandboxed-module');
-
-var user = SandboxedModule.require('./user', {
-  requires: {'mysql': {fake: 'mysql module'}},
-  globals: {myGlobal: 'variable'},
-  locals: {myLocal: 'other variable'},
-});
-
